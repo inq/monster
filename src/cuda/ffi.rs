04863ca017439a95d620b0a2aaba2bae5a1a2c49
inkyu <gofiri@gmail.com>
2016-04-26 16:34:20 +0900

    separated ffi functions

---
+++ src/cuda/ffi.rs
@@ -0,0 +1,28 @@
+#[allow(dead_code)]
+#[repr(C)]
+pub enum Error {
+    Success = 0,
+    ErrorInvalidValue = 1,
+}
+
+#[allow(dead_code)]
+#[repr(C)]
+pub enum MemcpyKind {
+    HostToHost = 0,
+    HostToDevice = 1,
+    DeviceToHost = 2,
+    DeviceToDevice = 3,
+    Default = 4
+}
+
+extern "C" {
+    pub fn cudaMalloc(devPtr: *mut *mut ::libc::c_void,
+                      size: ::libc::c_ulong)
+        -> Error;
+
+    pub fn cudaMemcpy(dst: *mut ::libc::c_void,
+                      src: *const ::libc::c_void,
+                      count: ::libc::c_ulong,
+                      kind: MemcpyKind)
+        -> Error;
+}
--- src/cuda/mod.rs
+++ src/cuda/mod.rs
@@ -1,32 +1,6 @@
-use std::mem;
+mod ffi;
 
 #[allow(dead_code)]
-pub enum Error {
-    Success = 0,
-    ErrorInvalidValue = 1,
-}
-
-#[allow(dead_code)]
-pub enum MemcpyKind {
-    HostToHost = 0,
-    HostToDevice = 1,
-    DeviceToHost = 2,
-    DeviceToDevice = 3,
-    Default = 4
-}
-
-extern "C" {
-    pub fn cudaMalloc(devPtr: *mut *mut ::libc::c_void,
-                      size: ::libc::c_ulong)
-        -> Error;
-
-    pub fn cudaMemcpy(dst: *mut ::libc::c_void,
-                      src: *const ::libc::c_void,
-                      count: ::libc::c_ulong,
-                      kind: MemcpyKind)
-        -> Error;
-}
-
 pub struct Memory<T> {
     pub data: *mut ::libc::c_void,
     size: usize,
@@ -36,7 +10,7 @@ pub struct Memory<T> {
 impl<T> Memory<T> {
     pub fn new(size: usize) -> Memory<T> {
         let mut data: *mut ::libc::c_void = ::std::ptr::null_mut();
-        let err = unsafe { cudaMalloc(&mut data, (size * ::std::mem::size_of::<T>()) as u64) };
+        let err = unsafe { ffi::cudaMalloc(&mut data, (size * ::std::mem::size_of::<T>()) as u64) };
         Memory::<T> {
             data: data,
             size: size,
@@ -46,19 +20,19 @@ impl<T> Memory<T> {
 
     pub fn write(&self, data: &Vec<T>) {
         unsafe {
-            cudaMemcpy(self.data,
+            ffi::cudaMemcpy(self.data,
                        data.as_ptr() as *mut ::libc::c_void,
                        (data.len() * ::std::mem::size_of::<T>()) as u64,
-                       MemcpyKind::HostToDevice);
+                       ffi::MemcpyKind::HostToDevice);
         }
     }
 
     pub fn read(&self, data: &Vec<T>) {
         unsafe {
-            cudaMemcpy(data.as_ptr() as *mut ::libc::c_void,
+            ffi::cudaMemcpy(data.as_ptr() as *mut ::libc::c_void,
                        self.data,
                        (data.len() * ::std::mem::size_of::<T>()) as u64,
-                       MemcpyKind::DeviceToHost);
+                       ffi::MemcpyKind::DeviceToHost);
         }
     }
 }
