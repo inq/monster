57ff19524e380ba57f8d3d25a4b19bd86d89113f
inkyu <gofiri@gmail.com>
2016-05-09 20:42:11 +0900

    simplified the namespace usage

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,27 @@ mod cudnn;
 mod cifar;
 mod util;
 
+use cudnn::{Cudnn, Tensor};
+use cudart::{Memory};
+use util::{Image};
+use cifar::{Cifar};
 use std::process::exit;
 use std::path::Path;
 use std::env;
 
 fn run(args: Vec<String>) -> Result<(), &'static str> {
-    let cudnn = try!{ cudnn::Cudnn::new() };
+    let cudnn = try!{ Cudnn::new() };
 
-    let cifar = cifar::Cifar::new(&args[1]);
+    let cifar = Cifar::new(&args[1]);
     let image = cifar.images.iter().nth(9999).unwrap();
 
     // alloc device memory
-    let _src_desc = try! { cudnn::Tensor::new_4d(1, 3, 32, 32) };
-    let _dst_desc = try! { cudnn::Tensor::new_4d(1, 3, 32, 32) };
-    let mut dst = try! { cudart::Memory::<f32>::new(32 * 32 * 3) };
+    let _src_desc = try! { Tensor::new_4d(1, 3, 32, 32) };
+    let _dst_desc = try! { Tensor::new_4d(1, 3, 32, 32) };
+    let mut dst = try! { Memory::<f32>::new(32 * 32 * 3) };
     let src = try! { image.to_device() };
     try! { cudnn.sigmoid_forward(_src_desc, &src, _dst_desc, &mut dst) };
-    let img = try! { util::Image::from_device(dst, 1u8, 32, 32) };
+    let img = try! { Image::from_device(dst, 1u8, 32, 32) };
 
     // write png image
     img.save("images/cifar.png")
