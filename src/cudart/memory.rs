56ce763b8d0fc51bf2eea9d890a5caa2d69e18af
inkyu <gofiri@gmail.com>
2016-05-12 16:53:46 +0900

    cleanup memory codes

--- src/cudart/memory.rs
+++ src/cudart/memory.rs
@@ -1,16 +1,19 @@
 use cudart::ffi;
+use libc::c_void;
+use std::ptr::null_mut;
+use std::mem::size_of;
 
 #[allow(dead_code)]
 pub struct Memory<T> {
-    pub data: *mut ::libc::c_void,
+    pub data: *mut c_void,
     size: usize,
     dummy: [T; 0]
 }
 
 impl<T> Memory<T> {
     pub fn new(size: usize) -> Result<Memory<T>, &'static str> {
-        let mut data: *mut ::libc::c_void = ::std::ptr::null_mut();
-        match unsafe { ffi::cudaMalloc(&mut data, (size * ::std::mem::size_of::<T>()) as u64) } {
+        let mut data: *mut c_void = null_mut();
+        match unsafe { ffi::cudaMalloc(&mut data, (size * size_of::<T>()) as u64) } {
             ffi::Error::Success => Ok(Memory::<T> {
                 data: data,
                 size: size,
@@ -22,8 +25,8 @@ impl<T> Memory<T> {
 
     pub fn write(&self, data: &Vec<T>) -> Result<(), &'static str> {
         match unsafe { ffi::cudaMemcpy(self.data,
-                                       data.as_ptr() as *mut ::libc::c_void,
-                                       (data.len() * ::std::mem::size_of::<T>()) as u64,
+                                       data.as_ptr() as *mut c_void,
+                                       (data.len() * size_of::<T>()) as u64,
                                        ffi::MemcpyKind::HostToDevice) } {
             ffi::Error::Success => Ok(()),
             e => Err(e.to_str())
@@ -31,9 +34,9 @@ impl<T> Memory<T> {
     }
 
     pub fn read(&self, data: &Vec<T>) -> Result<(), &'static str> {
-        match unsafe { ffi::cudaMemcpy(data.as_ptr() as *mut ::libc::c_void,
+        match unsafe { ffi::cudaMemcpy(data.as_ptr() as *mut c_void,
                                        self.data,
-                                       (data.len() * ::std::mem::size_of::<T>()) as u64,
+                                       (data.len() * size_of::<T>()) as u64,
                                        ffi::MemcpyKind::DeviceToHost) } {
             ffi::Error::Success => Ok(()),
             e => Err(e.to_str())
