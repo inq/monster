985db98e4bb16e1f437af8a2c181563cb30cfc1e
inkyu <gofiri@gmail.com>
2016-05-13 13:50:42 +0900

    separated conv/relu from cudnn

--- src/cudnn/conv.rs
+++ src/cudnn/conv.rs
@@ -1,6 +1,6 @@
-use Tensor;
-use cudnn::Filter4d;
-use cudnn::ffi;
+use nn::Tensor;
+use cudnn::{ffi, Cudnn, Filter4d};
+use cudart::Memory;
 use std::ptr;
 
 pub struct Convolution2d {
@@ -55,5 +55,79 @@ impl Convolution2d {
             ffi::Status::Success => Ok((n, c, h, w)),
             e => Err(e.to_str())
         }
+    }
+}
+
+impl Cudnn {
+    pub fn get_conv_forward_algo(&self,
+                                 x: &Tensor, w: &Filter4d,
+                                 conv: &Convolution2d, y: &Tensor)
+                                 -> Result<ffi::ConvolutionFwdAlgo, &'static str> {
+        let mut res = ffi::ConvolutionFwdAlgo::ImplicitGemm;
+        match unsafe { ffi::cudnnGetConvolutionForwardAlgorithm(self.handle,
+                                                                x.desc, w.desc, conv.desc, y.desc,
+                                                                ffi::ConvolutionFwdPreference::NoWorkspace,
+                                                                0,
+                                                                &mut res) } {
+            ffi::Status::Success => Ok(res),
+            e => Err(e.to_str())
+        }
+    }
+
+    pub fn get_conv_forward_workspace_size(&self,
+                                           x: &Tensor,
+                                           filter: &Filter4d,
+                                           conv: &Convolution2d,
+                                           y: &Tensor,
+                                           algo: ffi::ConvolutionFwdAlgo)
+                                           -> Result<usize, &'static str> {
+
+        let mut res = 0usize;
+        match unsafe { ffi::cudnnGetConvolutionForwardWorkspaceSize(self.handle,
+                                                                      x.desc,
+                                                                      filter.desc,
+                                                                      conv.desc,
+                                                                      y.desc,
+                                                                      algo,
+                                                                      &mut res) } {
+            ffi::Status::Success => Ok(res),
+            e => Err(e.to_str())
+        }
+    }
+
+    pub fn conv_forward(&self,
+                        x: &Tensor,
+                        filter: &Filter4d,
+                        w: &Tensor,
+                        conv: &Convolution2d,
+                        y: &Tensor)
+                        -> Result<(), &'static str> {
+        let alpha = 1f32;
+        let beta = 0f32;
+        let algo = try!(self.get_conv_forward_algo(&x,
+                                                   &filter,
+                                                   &conv,
+                                                   &y));
+        let workspace_size = try!(self.get_conv_forward_workspace_size(&x,
+                                                                       &filter,
+                                                                       &conv,
+                                                                       &y,
+                                                                       algo));
+        let workspace = try!(Memory::<f32>::new(workspace_size / 4));
+        unsafe {
+            ffi::cudnnConvolutionForward(self.handle,
+                                         &alpha as *const _ as *const ::libc::c_void,
+                                         x.desc,
+                                         x.data,
+                                         w.desc,
+                                         w.data,
+                                         conv.desc,
+                                         algo,
+                                         workspace.data,
+                                         workspace_size,
+                                         &beta as *const _ as *const ::libc::c_void,
+                                         y.desc,
+                                         y.data)
+        }.to_result()
     }
 }
